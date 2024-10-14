fe1414d7ffb42500d20014ccd1be842bdf009175
V0idpool <155442279+V0idpool@users.noreply.github.com>
2024-10-13 22:07:10 -0500

    Add files via upload
    Initial Commit

---
+++ VoidMail (ModMail)/NSComboItem.cs
@@ -0,0 +1,15 @@
+﻿namespace VoidMail
+{
+    // Helper for the terrible nsComboBoxes
+    internal class NSComboItem
+    {
+        private string name;
+
+        public NSComboItem(string name)
+        {
+            this.name = name;
+        }
+
+        public ulong Tag { get; set; }
+    }
+}
